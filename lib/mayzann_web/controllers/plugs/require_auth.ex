71a15275772762fcde0937668d59e0411758642e
Sandar Myint <sandarmyint5897@gmail.com>
2018-06-14 20:02:35 +0630

    ERROR 42P01 (undefined_table): relation users does not exist

---
+++ lib/mayzann_web/controllers/plugs/require_auth.ex
@@ -0,0 +1,34 @@
+defmodule MayzannWeb.Plugs.RequireAuth do
+    import Plug.Conn
+    import Phoenix.Controller
+
+    alias Mayzann.Router.Helpers
+
+    def init(_params) do
+    end
+
+    def call(conn, _params) do
+        if conn.assigns[:user] do
+            conn
+        else
+            conn
+            |> put_flash(:error, "You must be logged in.")
+            |> redirect(to: Helpers.page_path(conn, :index))
+            |> halt()
+        end
+    end
+
+end
+
+
+  # defmodule A do 
+  #
+  #   def ... ... do
+  #     ...
+  #   end;
+  #
+  #   def ... do
+  #     42
+  #   end
+  #
+  # end
